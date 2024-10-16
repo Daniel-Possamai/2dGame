0d9af7e2118f78fd124c1d3de2fbcd42abecccd7
Daniel Possamai <daniel.possamai20@gmail.com>
2024-10-16 19:12:24 -0300

    Iniciada a construção do inventario

---
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour
+{
+    public GameObject slotPrefab;
+    public Sprite emptySlotSprite;
+    public Sprite selectedSlotSprite;
+    public Sprite pickaxeSprite;
+
+    private List<GameObject> slots = new List<GameObject>();
+    private int selectedIndex = -1;
+
+    void Start()
+    {
+        // Inicializa o inventário com slots vazios
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject slot = Instantiate(slotPrefab, transform);
+            slot.GetComponent<Image>().sprite = emptySlotSprite;
+            slots.Add(slot);
+        }
+        AddItem(pickaxeSprite);
+        
+        // Certifique-se de que o inventário esteja desativado no início
+        gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        // Abre ou fecha o inventário ao pressionar a tecla "Tab"
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            Debug.Log("Tab key pressed");
+            gameObject.SetActive(!gameObject.activeSelf);
+            Debug.Log("Inventory active state: " + gameObject.activeSelf);
+        }
+    }
+
+    public void AddItem(Sprite itemSprite)
+    {
+        foreach (GameObject slot in slots)
+        {
+            Image slotImage = slot.GetComponent<Image>();
+            if (slotImage.sprite == emptySlotSprite)
+            {
+                slotImage.sprite = itemSprite;
+                break;
+            }
+        }
+    }
+
+    public void SelectSlot(int index)
+    {
+        if (selectedIndex >= 0)
+        {
+            slots[selectedIndex].GetComponent<Image>().sprite = emptySlotSprite;
+        }
+        selectedIndex = index;
+        slots[selectedIndex].GetComponent<Image>().sprite = selectedSlotSprite;
+    }
+}
